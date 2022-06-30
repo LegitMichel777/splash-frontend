64ece72a3cd354667b7012316029993089ec34af
Michel <legitmichel777@outlook.com>
2022-06-30 22:51:34 +0800

    Filled in production data for the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,15 +8,15 @@ const Footer = ({ styles }) => {
                 <div id={"upper-footer"}>
                     <div id={"footer-inquiries"}>Any inquiries? Contact us at team@splashstem.org</div>
                     <div id={"footer-social-media"}>
-                        <a href={"https://www.youtube.com"} className={"footer-social-media-item"}>
+                        <a href={"https://www.youtube.com/channel/UC_RFGCjgFzJZjMvd6rFyLeQ"} className={"footer-social-media-item"}>
                             <img src={"/images/icons/social-media/youtube-icon.png"} style={{width: 40, height: 28}}/>
                         </a>
-                        <a href={"https://www.instagram.com"} className={"footer-social-media-item"}>
+                        <a href={"https://www.instagram.com/splashstem/"} className={"footer-social-media-item"}>
                             <img src={"/images/icons/social-media/instagram-icon.png"} style={{width: 28, height: 28}}/>
                         </a>
-                        <a href={"https://www.twitter.com"} >
-                            <img src={"/images/icons/social-media/twitter-icon.png"} style={{width: 34, height: 28}}/>
-                        </a>
+                        {/*<a href={"https://www.twitter.com"} >*/}
+                        {/*    <img src={"/images/icons/social-media/twitter-icon.png"} style={{width: 34, height: 28}}/>*/}
+                        {/*</a>*/}
                     </div>
                 </div>
                 <div id={"footer-copyright"}>© 2022 SPLASH. All Rights Reserved.</div>
